6843881d724a0857f6ad09f0625f4d9b590911e8
Vaibhav Diwan <divaibhav@gmail.com>
2021-02-22 12:00:47 +0530

    update comment in MyMian.java

--- src/main/MyMain.java
+++ src/main/MyMain.java
@@ -5,8 +5,8 @@ import infix.postfix.InfixToPostfix;
 public class MyMain {
     public static void main(String[] args) {
         String infixString ="A+B-C*D";
-                
-        //"A/C^L*(N*M/O)^G*F";
+
+        //input infix = "A/C^L*(N*M/O)^G*F";
         // expected output --> A C L ^ / N M * O / G ^ * F *
         //output --> postfix = ACL^/NM*O/G^*F*
         InfixToPostfix convertor = new InfixToPostfix();
